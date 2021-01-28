842321f88efbf08487dc55eec3d23c76cdf6739b
ploktor <71982526+ploktor@users.noreply.github.com>
2021-01-28 12:53:18 -0300

    cambios jueves

---
+++ Assets/Scripts/TimerButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerButton : MonoBehaviour
+{
+    public GameObject timerCircle;
+    public float timer = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("TimerCircleOff", timer);
+    }
+
+    void TimerCircleOff()
+    {
+        timerCircle.SetActive(false);
+    }
+}
